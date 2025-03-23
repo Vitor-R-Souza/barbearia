dc7b44816d4d8e55c620a28e925260a9affb734b
Vitor_R_De_Souza <101133891+Vitor-R-Souza@users.noreply.github.com>
2025-03-23 18:19:27 -0300

    schedulePage half done

--- src/app/schedules/components/schedule-calendar/schedule-calendar.component.ts
+++ src/app/schedules/components/schedule-calendar/schedule-calendar.component.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatCalendar } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
+import { MatTimepickerModule } from '@angular/material/timepicker';
 
 @Component({
   selector: 'app-schedule-calendar',
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatIconModule,
+    MatTimepickerModule,
+    MatSelectModule,
+    MatCalendar,
+    MatCardModule,
+    MatTableModule,
+    MatPaginatorModule,
+  ],
   templateUrl: './schedule-calendar.component.html',
-  styleUrl: './schedule-calendar.component.scss'
+  styleUrl: './schedule-calendar.component.scss',
 })
 export class ScheduleCalendarComponent {
-
+  displayedColumns: any;
+  onSubmit(_t5: any) {
+    throw new Error('Method not implemented.');
+  }
+  newSchedule: any;
+  onTimeChange($event: Event) {
+    throw new Error('Method not implemented.');
+  }
+  selected: any;
+  dataSource: any;
+  requestDelete(_t80: any) {
+    throw new Error('Method not implemented.');
+  }
 }
--- src/app/schedules/schedules-month/schedules-month.component.ts
+++ src/app/schedules/schedules-month/schedules-month.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { ScheduleCalendarComponent } from '../components/schedule-calendar/schedule-calendar.component';
 
 @Component({
   selector: 'app-schedules-month',
-  imports: [],
+  imports: [ScheduleCalendarComponent],
   templateUrl: './schedules-month.component.html',
-  styleUrl: './schedules-month.component.scss'
+  styleUrl: './schedules-month.component.scss',
 })
 export class SchedulesMonthComponent {
-
+  monthSchedule: any;
+  clients: any;
+  onDateChange($event: Event) {
+    throw new Error('Method not implemented.');
+  }
+  onConfirmDelete($event: Event) {
+    throw new Error('Method not implemented.');
+  }
+  onSchedueClient($event: Event) {
+    throw new Error('Method not implemented.');
+  }
 }

93e7568d5320661b24e6c748bbe471aed9fc2a67
Vitor_R_De_Souza <101133891+Vitor-R-Souza@users.noreply.github.com>
2025-03-24 15:57:19 -0300

    final comit

--- src/app/schedules/components/schedule-calendar/schedule-calendar.component.ts
+++ src/app/schedules/components/schedule-calendar/schedule-calendar.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  AfterViewInit,
+  Component,
+  EventEmitter,
+  Inject,
+  Input,
+  OnChanges,
+  OnDestroy,
+  Output,
+  SimpleChanges,
+  ViewChild,
+} from '@angular/core';
+import { FormControl, FormsModule, NgForm } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { MatCalendar } from '@angular/material/datepicker';
+import { MatCalendar, MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
-import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatSelectModule } from '@angular/material/select';
-import { MatTableModule } from '@angular/material/table';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatTimepickerModule } from '@angular/material/timepicker';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { SERVICES_TOKEN } from '../../../services/service.token';
+import { DialogManagerService } from '../../../services/dialog-manager.service';
+import {
+  ClientScheduleAppointmentModel,
+  SaveScheduleModel,
+  ScheduleAppointmentMonthModel,
+  SelectClientModel,
+} from '../../schedule.models';
+import { IDialogManagerService } from '../../../services/idialog-manager.service';
+import { YesNoDialogComponent } from '../../../commons/components/yes-no-dialog/yes-no-dialog.component';
+import { Subscription } from 'rxjs';
+import { provideNativeDateAdapter } from '@angular/material/core';
 
 @Component({
   selector: 'app-schedule-calendar',
@@ -19,22 +47,165 @@ import { MatTimepickerModule } from '@angular/material/timepicker';
     MatCardModule,
     MatTableModule,
     MatPaginatorModule,
+    CommonModule,
+    FormsModule,
+    MatDatepickerModule,
+    MatButtonModule,
+    MatTooltipModule,
+    MatInputModule,
+  ],
+  providers: [
+    provideNativeDateAdapter(),
+    { provide: SERVICES_TOKEN.DIALOG, useClass: DialogManagerService },
   ],
   templateUrl: './schedule-calendar.component.html',
   styleUrl: './schedule-calendar.component.scss',
 })
-export class ScheduleCalendarComponent {
-  displayedColumns: any;
-  onSubmit(_t5: any) {
-    throw new Error('Method not implemented.');
+export class ScheduleCalendarComponent
+  implements AfterViewInit, OnChanges, OnDestroy
+{
+  displayedColumns: string[] = ['startAt', 'endAt', 'client', 'actions'];
+
+  private _selected: Date = new Date();
+
+  private subscription?: Subscription;
+
+  dataSource!: MatTableDataSource<ClientScheduleAppointmentModel>;
+
+  addingSchedule: boolean = false;
+
+  newSchedule: SaveScheduleModel = {
+    startAt: undefined,
+    endAt: undefined,
+    clientId: undefined,
+  };
+
+  clientSelectFormControll = new FormControl();
+
+  @Input()
+  monthSchedule!: ScheduleAppointmentMonthModel;
+
+  @Input()
+  clients: SelectClientModel[] = [];
+
+  @Output()
+  onDateChange = new EventEmitter<Date>();
+
+  @Output()
+  onConfirmDelete = new EventEmitter<ClientScheduleAppointmentModel>();
+
+  @Output()
+  onScheduleClient = new EventEmitter<SaveScheduleModel>();
+
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
+
+  constructor(
+    @Inject(SERVICES_TOKEN.DIALOG)
+    private readonly dialogManagerService: IDialogManagerService
+  ) {}
+
+  get selected(): Date {
+    return this._selected;
   }
-  newSchedule: any;
-  onTimeChange($event: Event) {
-    throw new Error('Method not implemented.');
+
+  set selected(selected: Date) {
+    if (this._selected.getTime() !== selected.getTime()) {
+      this.onDateChange.emit(selected);
+      this.buildTable();
+      this._selected = selected;
+    }
   }
-  selected: any;
-  dataSource: any;
-  requestDelete(_t80: any) {
-    throw new Error('Method not implemented.');
+
+  ngOnInit(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
+
+  ngAfterViewInit(): void {
+    if (this.dataSource && this.paginator) {
+      this.dataSource.paginator = this.paginator;
+    }
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['monthSchedule'] && this.monthSchedule) {
+      this.buildTable();
+    }
+  }
+
+  onSubmit(form: NgForm) {
+    const startAt = new Date(this._selected);
+    const endAt = new Date(this._selected);
+    startAt.setHours(
+      this.newSchedule.startAt!.getHours(),
+      this.newSchedule.startAt!.getMinutes()
+    );
+    endAt.setHours(
+      this.newSchedule.endAt!.getHours(),
+      this.newSchedule.endAt!.getMinutes()
+    );
+    const saved: ClientScheduleAppointmentModel = {
+      id: -1,
+      day: this._selected.getDate(),
+      startAt,
+      endAt,
+      clientId: this.newSchedule.clientId!,
+      clientName: this.clients.find((c) => c.id === this.newSchedule.clientId!)!
+        .name,
+    };
+    this.monthSchedule.scheduledAppointments.push(saved);
+    this.onScheduleClient.emit(saved);
+    this.buildTable();
+    form.resetForm();
+    this.newSchedule = {
+      startAt: undefined,
+      endAt: undefined,
+      clientId: undefined,
+    };
+  }
+
+  requestDelete(schedule: ClientScheduleAppointmentModel) {
+    this.subscription = this.dialogManagerService
+      .showYesNoDialog(YesNoDialogComponent, {
+        title: 'exclusão do agendamento',
+        content: 'confirma a exclusão?',
+      })
+      .subscribe((result) => {
+        if (result) {
+          this.onConfirmDelete.emit(schedule);
+          const updatedList = this.dataSource.data.filter(
+            (c) => c.id !== schedule.id
+          );
+          this.dataSource =
+            new MatTableDataSource<ClientScheduleAppointmentModel>(updatedList);
+          if (this.paginator) {
+            this.dataSource.paginator = this.paginator;
+          }
+        }
+      });
+  }
+
+  onTimeChange(time: Date) {
+    const endAt = new Date(time);
+    endAt.setHours(time.getHours() + 1);
+    this.newSchedule.endAt = endAt;
+  }
+
+  private buildTable() {
+    const appointments = this.monthSchedule.scheduledAppointments.filter(
+      (a) => {
+        this.monthSchedule.year === this._selected.getFullYear() &&
+          this.monthSchedule.month - 1 === this._selected.getMonth() &&
+          a.day === this._selected.getDate();
+      }
+    );
+    this.dataSource = new MatTableDataSource<ClientScheduleAppointmentModel>(
+      appointments
+    );
+    if (this.paginator) {
+      this.dataSource.paginator = this.paginator;
+    }
   }
 }
--- src/app/schedules/schedules-month/schedules-month.component.ts
+++ src/app/schedules/schedules-month/schedules-month.component.ts
@@ -1,22 +1,95 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
 import { ScheduleCalendarComponent } from '../components/schedule-calendar/schedule-calendar.component';
+import { SERVICES_TOKEN } from '../../services/service.token';
+import { ISchdeuleService } from '../../services/api-client/schedules/ISchedule.service';
+import { ICLientService } from '../../services/api-client/clients/iclients.service';
+import { ISnackbarManagerService } from '../../services/isnackbar-manager.service';
+import { ClientsService } from '../../services/api-client/clients/clients.service';
+import { SchedulesService } from '../../services/api-client/schedules/schedules.service';
+import { SnackbarManagerService } from '../../services/snackbar-manager.service';
+import { share, Subscription } from 'rxjs';
+import {
+  ClientScheduleAppointmentModel,
+  SaveScheduleModel,
+  ScheduleAppointmentMonthModel,
+  SelectClientModel,
+} from '../schedule.models';
+import { SavedScheduleRequest } from '../../services/api-client/schedules/schedule.models';
 
 @Component({
   selector: 'app-schedules-month',
   imports: [ScheduleCalendarComponent],
   templateUrl: './schedules-month.component.html',
   styleUrl: './schedules-month.component.scss',
+  providers: [
+    { provide: SERVICES_TOKEN.HTTP.CLIENT, useClass: ClientsService },
+    { provide: SERVICES_TOKEN.HTTP.SCHEDULE, useClass: SchedulesService },
+    { provide: SERVICES_TOKEN.SNACKBAR, useClass: SnackbarManagerService },
+  ],
 })
-export class SchedulesMonthComponent {
-  monthSchedule: any;
-  clients: any;
-  onDateChange($event: Event) {
-    throw new Error('Method not implemented.');
+export class SchedulesMonthComponent implements OnInit, OnDestroy {
+  monthSchedule!: ScheduleAppointmentMonthModel;
+
+  clients: SelectClientModel[] = [];
+
+  private subscriptions: Subscription[] = [];
+
+  private selectedDate?: Date;
+
+  constructor(
+    @Inject(SERVICES_TOKEN.HTTP.SCHEDULE)
+    private readonly httpService: ISchdeuleService,
+    @Inject(SERVICES_TOKEN.HTTP.CLIENT)
+    private readonly clientHttpService: ICLientService,
+    @Inject(SERVICES_TOKEN.SNACKBAR)
+    private readonly snackBarManager: ISnackbarManagerService
+  ) {}
+
+  ngOnInit(): void {
+    this.fecthSchedules(new Date());
+    this.subscriptions.push(
+      this.clientHttpService.list().subscribe((data) => (this.clients = data))
+    );
   }
-  onConfirmDelete($event: Event) {
-    throw new Error('Method not implemented.');
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((s) => s.unsubscribe());
   }
-  onSchedueClient($event: Event) {
-    throw new Error('Method not implemented.');
+
+  onDateChange(date: Date) {
+    this.selectedDate = date;
+    this.fecthSchedules(date);
+  }
+
+  onConfirmDelete(schedule: ClientScheduleAppointmentModel) {
+    this.subscriptions.push(this.httpService.delete(schedule.id).subscribe());
+  }
+
+  onSchedueClient(schedule: SaveScheduleModel) {
+    if (schedule.startAt && schedule.endAt && schedule.clientId) {
+      const request: SavedScheduleRequest = {
+        startAt: schedule.startAt,
+        endAt: schedule.endAt,
+        clientId: schedule.clientId,
+      };
+      this.subscriptions.push(
+        this.httpService.save(request).subscribe(() => {
+          this.snackBarManager.show('agendamento concluido');
+          if (this.selectedDate) {
+            this.fecthSchedules(this.selectedDate);
+          }
+        })
+      );
+    }
+  }
+
+  private fecthSchedules(currentDate: Date) {
+    const year = currentDate.getFullYear();
+    const month = currentDate.getMonth() + 1;
+    this.subscriptions.push(
+      this.httpService
+        .listInMonth(year, month)
+        .subscribe((data) => (this.monthSchedule = data))
+    );
   }
 }

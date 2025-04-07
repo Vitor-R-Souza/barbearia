c5ece7b912e83e0163e14233ea178a4ba5961913
Vitor_R_De_Souza <101133891+Vitor-R-Souza@users.noreply.github.com>
2025-04-07 15:15:56 -0300

    location time change

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import {
@@ -7,6 +7,10 @@ import {
 } from '@angular/common/http';
 import { provideNgxMask } from 'ngx-mask';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import localePt from '@angular/common/locales/pt'
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localePt);
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -15,5 +19,6 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(withInterceptorsFromDi()),
     provideNgxMask({}),
     provideAnimationsAsync(),
+    { provide: LOCALE_ID, useValue: 'pt-BR' }
   ],
 };
